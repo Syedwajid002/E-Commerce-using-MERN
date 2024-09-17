880679f211f84be9b7a891ff0c019fbece2fdd8e
syedwajid002 <syedwajid002@gmail.com>
2024-09-17 13:07:32 +0530

    Responsive ui

--- client/src/Components/JavaScript/Body.jsx
+++ client/src/Components/JavaScript/Body.jsx
@@ -26,13 +26,13 @@ function Body() {
         </div>
         <Link to='/login'><button className='bg-blue-600 text-white rounded-md pr-3 pl-3 p-2 font-semibold m-2.5 md:hidden'>Login</button></Link>
         </div>
-        </div>
             <div className='md:hidden flex w-full justify-between list-none position-absolute bg-black  border-0 text-white p-2'>
                 <Link to='/Products/men'><li>Mens</li></Link>
                 <Link to='/Products/women'><li>Womens</li></Link>
                 <Link to='/Products/jewellary'><li>Jewellary</li></Link>
                 <Link to='/Products/electronics'><li>Electronics</li></Link>
             </div>
+        </div>
         <BoxesScroll ref={targetRef} />
         </div>
     )
