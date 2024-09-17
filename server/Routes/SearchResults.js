618e0711e596f57c4b5b99ad798ed9e85e425e25
syedwajid002 <syedwajid002@gmail.com>
2024-09-17 15:46:10 +0530

    Filtering Products1

--- server/Routes/SearchResults.js
+++ server/Routes/SearchResults.js
@@ -9,6 +9,7 @@ const SearchedQuery= async (req, res) => {
     if (!query) {
       return res.status(400).json({ error: 'Query parameter is required' });
     }
+    query=query.toLowerCase();
     const results = await Products.find()
     const searcheddata = results.filter(item => item.category.toLowerCase().includes(query))
     res.json({ result: searcheddata });
